2dd8aa9859bd98cff47df63e0833f9c8c4ef561a
Cody Clark <codylcl@yahoo.com>
2022-10-25 18:12:14 -1000

    working post route

--- controllers/retros.js
+++ controllers/retros.js
@@ -2,9 +2,30 @@ const express = require('express')
 const retroRouter = express.Router()
 const retroData = require('../models/seed')
 const Retro = require('../models/retro.js')
+console.log(Retro)
+// Index Route
+retroRouter.get("/", (req, res) => {
+    Retro.find({}, (error, allRetros) => {
+      res.render("index.ejs", {
+        retros: allRetros,
+      })
+    })
+  })
 
+// New Route
 retroRouter.get('/new', (req, res) => {
     res.render('new.ejs')
+})
+
+// Create Route
+retroRouter.post('/', (req, res) => {
+    Retro.create(req.body, (error, createdRetro) => {
+        console.log(createdRetro)
+        console.log(req.body)
+        console.log(error)
+        res.redirect('/retro')
+
+    })
 })
 
 module.exports = retroRouter
--- models/retro.js
+++ models/retro.js
@@ -4,10 +4,10 @@ const retroSchema = new mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
     img: {type: String, required: true},
-    price: {type: Number, required: true},
+    price: {type: String, required: true},
     qty: {type: Number, required: true}
 })
 
-const Retro = mongoose.model('retro', retroSchema)
+const Retro = mongoose.model('Retro', retroSchema)
 
 module.exports = Retro
