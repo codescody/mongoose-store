d516dddba63686103f1b7c6c0b5064e5c8d85f36
Cody Clark <codylcl@yahoo.com>
2022-10-25 16:13:37 -1000

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+app.listen(PORT)
