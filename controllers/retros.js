e40ba5b40a3de1d22d06c64c223b94a691dfb07d
Cody Clark <codylcl@yahoo.com>
2022-10-26 16:01:52 -1000

    working put route and edit page

--- controllers/retros.js
+++ controllers/retros.js
@@ -17,11 +17,34 @@ retroRouter.get('/new', (req, res) => {
     res.render('new.ejs')
 })
 
+// Update Route
+retroRouter.put("/:id", (req, res) => {
+  Retro.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+    (error, updatedRetro) => {
+      res.redirect(`/retro/${req.params.id}`)
+    }
+    )
+})
+
 // Create Route
 retroRouter.post('/', (req, res) => {
     Retro.create(req.body, (error, createdRetro) => {
         res.redirect('/retro')
     })
+})
+
+// Edit Route
+retroRouter.get("/:id/edit", (req, res) => {
+  Retro.findById(req.params.id, (error, foundRetro) => {
+    res.render("edit.ejs", {
+      retro: foundRetro,
+    })
+  })
 })
 
 // Show Route
